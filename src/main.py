9f31ff5f1547396fbb25b46fba4981bceafb8150
BuildTools <unconfigured@null.spigotmc.org>
2015-06-18 10:40:16 -0400

    20 humans
    creates and updates a 20-person population

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
-print("This is an awesome project!")
+from math import *
+from random import *
+from graphics import *
+import time
+
+#init
+people = []
+population = 20
+size = 50
+
+#create our population
+for i in range(population):
+    people.append(Human(size * random(), size * random()))
+
+#update accordingly
+for i in range(population):
+    people[i].update()
