3e2760e1d35b87154432350a0dda29494bcf9d73
BuildTools <unconfigured@null.spigotmc.org>
2015-06-18 10:23:34 -0400

    human.py

---
+++ src/human.py
@@ -0,0 +1,28 @@
+# human.py
+
+from math import *
+from graphics import *
+from random import *
+import time
+
+#############################################
+#   Traits  |   All values are from 0 to 1  #
+#-------------------------------------------#
+#   Individualism   vs      Collectivism    # 
+#   Sociability     vs      Survivalist     # 
+#   Wealthy         vs      Needy           #
+#   Smart           vs      Not-So-Smart    # 
+#   Leader          vs      Follower        # 
+#   Aggression      vs      Pacifism        # 
+#   Anarchist       vs      Communist       # 
+#############################################
+
+class Human(object):
+    
+    social = 1 - (random() * random())
+
+    def social(self):
+        return self.social
+
+    def update(self):
+        return 0
